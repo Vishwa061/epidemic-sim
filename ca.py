e887bab39984b2b190fa12b6d2cd44181d3b0fe8
Vishwa Perera <vperera@mrscompany.com>
2022-04-06 13:32:29 -0400

    added vaccination by collective action

--- ca.py
+++ ca.py
@@ -1,40 +1,33 @@
 import random
-from turtle import color
 import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
+SEED = 109875089
+random.seed(SEED)
+
 class State:
     SUSCEPTIBLE = 0
     INFECTED = 1
-    REMOVED = 2
+    RECOVERED = 2
+    VACCINATED = 3
+
 
 class Node:
+    COLOR_MAP = ['white', 'red', 'black', 'yellow']
+
     def __init__(self, row, col, state=State.SUSCEPTIBLE):
         self.name = f'r{row}c{col}'
         self.row = row
         self.col = col
         self.state = state
         self.duration = 0 # how long the node has been in the current state (in time steps)
-
-    def __str__(self):
-        if self.state == State.SUSCEPTIBLE:
-            return '.'
-        elif self.state == State.INFECTED:
-            return '#'
-        elif self.state == State.REMOVED:
-            return 'x'
-        raise Exception(f'Invalid state: {self.state}')
+        self.adoption_threshold = random.random()
+        self.suseptiblity = random.random()
     
     def get_color(self):
-        if self.state == State.SUSCEPTIBLE:
-            return 'white'
-        elif self.state == State.INFECTED:
-            return 'red'
-        elif self.state == State.REMOVED:
-            return 'black'
-        raise Exception(f'Invalid state: {self.state}')
+        return Node.COLOR_MAP[self.state]
 
 
 class Grid:
@@ -42,9 +35,6 @@ class Grid:
         self.rows = rows
         self.cols = cols
         self.grid = [[Node(r, c) for c in range(cols)] for r in range(rows)]
-
-    def __str__(self):
-        return '\n'.join([' '.join([str(n) for n in row]) for row in self.grid])
 
     def get_neighbours(self, row, col, radius=1):
         neighbours = []
@@ -60,32 +50,34 @@ class Grid:
         col = int(name[1:].split('c')[1])
         return self.grid[row][col]
       
+
 class Simulation:
     WAVES_SAVE_FOLDER = 'ca_waves'
     EDGES_SAVE_FILENAME = 'ca_edges.csv'
     DEFAULT_INTERVAL = 200
     NODE_SIZE = 40
-    CHANCE_OF_INFECTION = 0.05 # 5% chance of infection, when greater than 4% it goes viral (infects more than half the pop)
+    CHANCE_OF_INFECTION = 0.1 # 10% chance of infection
+    RECOVERY_TIME = 14
 
-    def __init__(self, rows, cols, initial_infection_percent, frames=None, interval=None, seed=None):
+    def __init__(self, rows, cols, initial_infection_percent):
         self.rows = rows
         self.cols = cols
         self.grid = Grid(rows, cols)
         self.edges = [] # directed edges: (from node, to node)
         self.waves = [] # list of waves where each wave is a list of edges
-        self.removed = [] # list of removed nodes
         self.SIR_over_time = [] # list of (t, (num of S nodes, num of I nodes, num of R nodes)) tuples
-        self.time_steps = 1
-        self.interval = interval or self.DEFAULT_INTERVAL
-        self.frames = frames or self.frames_gen
-        self.seed = seed or random.randint(0, 1000000)
+        self.time_steps = 0
+        self.interval = Simulation.DEFAULT_INTERVAL
+        self.frames = self.frames_gen
         self.ani = None
+        self.vaccination_enabled = False
+        self.num_nodes = rows * cols
 
         # infect a percentage of the nodes
         self.do_initial_infection(initial_infection_percent)
 
         # create graph
-        self.G = nx.Graph(seed=self.seed)
+        self.G = nx.Graph(seed=SEED)
         self.fixed_positions = {}
         for row in self.grid.grid:
             for node in row:
@@ -102,6 +94,18 @@ class Simulation:
         # create figure
         self.fig, self.ax = plt.subplots(figsize=(8, 8))
 
+    def set_frames(self, frames):
+        self.frames = frames
+
+    def set_interval(self, interval):
+        self.interval = interval
+
+    def enable_vaccination(self):
+        self.vaccination_enabled = True
+
+    def disable_vaccination(self):
+        self.vaccination_enabled = False
+
     def do_initial_infection(self, percent_to_infect):
         for r in self.grid.grid:
             for n in r:
@@ -114,11 +118,17 @@ class Simulation:
             n.duration = 0
             self.G.nodes[n.name]['color'] = 'red'
     
-    def remove_nodes(self, nodes_to_recover):
+    def recover_nodes(self, nodes_to_recover):
         for n in nodes_to_recover:
-            n.state = State.REMOVED
+            n.state = State.RECOVERED
             n.duration = 0
             self.G.nodes[n.name]['color'] = 'black'
+
+    def vaccinate_nodes(self, nodes_to_vaccinate):
+        for n in nodes_to_vaccinate:
+            n.state = State.VACCINATED
+            n.duration = 0
+            self.G.nodes[n.name]['color'] = 'yellow'
 
     def increment_durations(self):
         for r in self.grid.grid:
@@ -126,33 +136,45 @@ class Simulation:
                 n.duration += 1
 
     def get_num_susceptible_nodes(self):
-        return len([n for n in self.grid.grid for n in n if n.state == State.SUSCEPTIBLE])
+        return len([n for n in self.grid.grid for n in n if n.state == State.SUSCEPTIBLE or n.state == State.VACCINATED])
 
     def get_num_infected_nodes(self):
         return len([n for n in self.grid.grid for n in n if n.state == State.INFECTED])
 
-    def get_num_removed_nodes(self):
-        return len([n for n in self.grid.grid for n in n if n.state == State.REMOVED])
+    def get_num_recovered_nodes(self):
+        return len([n for n in self.grid.grid for n in n if n.state == State.RECOVERED])
+
+    def get_num_vaccinated_nodes(self):
+        return len([n for n in self.grid.grid for n in n if n.state == State.VACCINATED])
 
     def step(self, current_time_step):
         nodes_to_infect = []
-        nodes_to_remove = []
+        nodes_to_recover = []
+        nodes_to_vaccinate = []
         wave = []
         for r in self.grid.grid:
             for n in r:
-                if n.state == State.INFECTED and n.duration >= 5: # remove after 5 time steps 
-                    nodes_to_remove.append(n)
+                if n.state == State.INFECTED and n.duration >= Simulation.RECOVERY_TIME: # recover after 14 time steps
+                    nodes_to_recover.append(n)
                 elif n.state == State.SUSCEPTIBLE:
+                    chance_of_infection = Simulation.CHANCE_OF_INFECTION * n.suseptiblity
+                    if self.vaccination_enabled:
+                        if n.state == State.SUSCEPTIBLE and n.adoption_threshold < (self.get_num_recovered_nodes() * 0.3 + self.get_num_vaccinated_nodes()) / self.num_nodes:
+                            nodes_to_vaccinate.append(n)
+                            continue
+                        if n.state == State.VACCINATED:
+                            chance_of_infection *= 0.1
                     neighbours = self.grid.get_neighbours(n.row, n.col)
                     for neighbour in neighbours:
-                        if neighbour.state == State.INFECTED and random.random() < self.CHANCE_OF_INFECTION:
+                        if neighbour.state == State.INFECTED and random.random() < chance_of_infection:
                             nodes_to_infect.append(n)
                             wave.append((neighbour, n))
                             self.edges.append((neighbour, n))
                             break
         self.increment_durations()
         self.infect_nodes(nodes_to_infect)
-        self.remove_nodes(nodes_to_remove)
+        self.recover_nodes(nodes_to_recover)
+        self.vaccinate_nodes(nodes_to_vaccinate)
         self.waves.append(wave)
         self.time_steps += 1
         self.display_graph(current_time_step)
@@ -164,8 +186,10 @@ class Simulation:
             if u.state == v.state:
                 if u.state == State.INFECTED:
                     self.G.edges[u.name, v.name]['color'] = 'red'
-                elif u.state == State.REMOVED:
+                elif u.state == State.RECOVERED:
                     self.G.edges[u.name, v.name]['color'] = 'black'
+                elif u.state == State.VACCINATED:
+                    self.G.edges[u.name, v.name]['color'] = 'yellow'
                 else:
                     self.G.edges[u.name, v.name]['color'] = 'green'
 
@@ -190,25 +214,19 @@ class Simulation:
             R0 = num_infected_edges / num_infectious_nodes
         R0_formated = str('{:10.4f}'.format(R0)).strip()
 
-        suseptible_nodes = 0
-        infected_nodes = 0
-        removed_nodes = 0
-        for c in node_color_map:
-            if c == 'red':
-                infected_nodes += 1
-            elif c == 'green':
-                suseptible_nodes += 1
-            else:
-                removed_nodes += 1
-        self.SIR_over_time.append((num, (suseptible_nodes, infected_nodes, removed_nodes)))
+        # record SIR over time
+        suseptible_nodes = self.get_num_susceptible_nodes()
+        infected_nodes = self.get_num_infected_nodes()
+        recovered_nodes = self.get_num_recovered_nodes()
+        self.SIR_over_time.append((num, (suseptible_nodes, infected_nodes, recovered_nodes)))
 
         self.ax.clear()
-        self.ax.set_title(f't = {str(num)} | R0 = {R0_formated} | S = {suseptible_nodes}, I = {infected_nodes}, R = {removed_nodes}', fontweight="bold")
+        self.ax.set_title(f't = {str(num)} | R0 = {R0_formated} | S = {suseptible_nodes}, I = {infected_nodes}, R = {recovered_nodes}', fontweight="bold")
         self.ax.set_xticks([])
         self.ax.set_yticks([])
-        nx.draw(G=self.G, pos=self.pos, ax=self.ax, node_size=self.NODE_SIZE, node_color=node_color_map, edge_color=edge_color_map)
+        nx.draw(G=self.G, pos=self.pos, ax=self.ax, node_size=Simulation.NODE_SIZE, node_color=node_color_map, edge_color=edge_color_map)
         
-        if num % 29 == 0:
+        if num % 10 == 0:
             plt.savefig(f'ca_propagation/ca_propagation{str(num)}.png', bbox_inches='tight')
 
     def frames_gen(self):
@@ -230,11 +248,11 @@ class Simulation:
                 f.write(line)
     
     def save_edges_to_csv(self):
-        self.save_to_csv(self.edges, self.EDGES_SAVE_FILENAME)
+        self.save_to_csv(self.edges, Simulation.EDGES_SAVE_FILENAME)
 
     def save_waves_to_csv(self):
         for i in range(len(self.waves)):
-            self.save_to_csv(self.waves[i], f'{self.WAVES_SAVE_FOLDER}/wave{i}.csv')
+            self.save_to_csv(self.waves[i], f'{Simulation.WAVES_SAVE_FOLDER}/wave{i}.csv')
 
     def analyze(self):
         # create infection graph
@@ -261,6 +279,7 @@ class Simulation:
         plt.title('SIR Over Time')
         plt.xlabel('Time steps')
         plt.ylabel('Number of Nodes')
+        plt.yscale('log')
         plt.legend(['Susceptible (S)', 'Infected (I)', 'Removed (R)'], loc='center right')
         plt.savefig(f'ca_analysis/SIR_over_time.png', bbox_inches='tight')
         plt.clf()
@@ -278,21 +297,21 @@ class Simulation:
             # diameter = nx.diameter(D)
             # print(f'Diameter of the graph with threshold {threshold} is {diameter}')
 
+
 if __name__ == '__main__':
     # sim parameters
     rows = 40
     cols = 40
     initial_infection_percent = 0.01
-    seed = 4561215534
-    frames = 100
-    interval = 200
+    frames = 121
+    interval = 1
 
-    # seed the random number generator
-    random.seed(seed)
-
-    # create and run the simulation
-    sim = Simulation(rows, cols, initial_infection_percent, interval=1, seed=seed)
+    # run the simulation without vaccinations
+    sim = Simulation(rows, cols, initial_infection_percent)
+    sim.set_frames(frames)
+    sim.set_interval(interval)
+    sim.enable_vaccination()
     sim.start()
 
     # run analysis
-    # sim.analyze()
+    sim.analyze()
